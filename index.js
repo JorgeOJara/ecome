c832ecc7ef685b667a4e3a929ca05fd072a7421f
jorgeojara <jorgeojara@gmail.com>
2024-10-06 10:59:05 -0400

    changing env values...

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const multer = require('multer');
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'rootpassword',
+    password: process.env.MYSQL_PASSWORD ,
     database: process.env.MYSQL_DATABASE || 'ecome_db'
 });
 
